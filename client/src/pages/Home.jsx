2b1870af4e3f34d0b2c408a13097392c7d011125
vedikakulkarni-23 <kulkarnivedika91@gmail.com>
2026-01-17 23:52:36 +0530

    Fix case-sensitive import for Netlify builds to deploy project.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Banner from '../Components/home/Banner'
 import Hero from '../Components/home/Hero'
-import Features from '../Components/home/features'
+import Features from '../Components/home/Features'
 import Testimonial from '../Components/home/Testimonial'
 import CallToAction from '../Components/home/CallToAction'
-import Footer from '../Components/home/footer'
+import Footer from '../Components/home/Footer'
 
 const Home = () => {
   return (
